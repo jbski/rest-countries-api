6592f0cf76cf8705709f7ee55b00bcd8fb154fa4
jbski <57382915+jbski@users.noreply.github.com>
2022-08-25 20:22:34 -0700

    add search country by name js

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ regions.forEach(function(region){
 document.getElementById('region').addEventListener('change', getRegionData);
 
 function getRegionData() {    
-    select = document.getElementById('region');    
+    select = document.getElementById('region'); 
+    //Get the value selected in the select box and assign it to region_value   
     region_value = select.options[select.selectedIndex].value;
-    console.log(region_value);    
-    region_api_base_path = 'https://restcountries.com/v3.1/region/'
-    //Remove countries from the prior region selection
+    //Define the base path of the API    
+    region_api_base_path = 'https://restcountries.com/v2/region/'
+    //Remove countries from the prior region selection from the page
     const articles = document.querySelectorAll('article');
     articles.forEach(function(article){
         article.remove();
@@ -31,16 +32,14 @@ function getRegionData() {
         })
         .then(function(region_data){                
             region_data.forEach(function(data){
-                let country = `${data.name.official}`;
+                let country = `${data.name}`;
                 let population = data.population;
                 let region = data.region;
-                let capital = data.capital[0];
+                let capital = data.capital;
                 let flag_url = data.flags.png
-                // Create main article for country
+                // Create main article tag for country
                 const article = document.createElement('article');
                 article.className = 'country-card';
-                // article.id = country;
-                // article.appendChild(document.createTextNode(`${country}`));
                 document.querySelector('div.articles-container').appendChild(article);
 
                 // Load flag image
@@ -58,7 +57,79 @@ function getRegionData() {
                 // Load country population
                 const div_population = document.createElement('div');
                 div_population.className = 'country-population'; 
-                div_population.appendChild(document.createTextNode(`${population}`));       
+                //Add "," in thousands place
+                div_population.appendChild(document.createTextNode(`${population.toLocaleString('en-US')}`));       
+                article.appendChild(div_population);
+
+                // Load country region
+                const div_region = document.createElement('div');
+                div_region.className = 'country-region'; 
+                div_region.appendChild(document.createTextNode(`${region}`));       
+                article.appendChild(div_region);
+
+                // Load country capital
+                const div_capital = document.createElement('div');
+                div_capital.className = 'country-capital'; 
+                div_capital.appendChild(document.createTextNode(`${capital}`));       
+                article.appendChild(div_capital);    
+            }) 
+        })        
+        .catch(function(err){
+            console.log(err);
+        });
+}
+
+
+
+// Fetch API call to search for country by name
+document.getElementById('country').addEventListener('input', getCountry);
+
+function getCountry(e) {    
+    select = document.getElementById('country'); 
+    //Get the value selected in the select box and assign it to region_value   
+    country_value = e.target.value;
+    console.log(country_value);
+    //Define the base path of the API    
+    country_api_base_path = 'https://restcountries.com/v2/name/'
+    //Remove countries from the prior region selection from the page
+    const articles = document.querySelectorAll('article');
+    articles.forEach(function(article){
+        article.remove();
+    })
+    //Get countries from the selected region
+    fetch(`${country_api_base_path}${country_value}`)
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(country_data){                
+            country_data.forEach(function(data){
+                let country = `${data.name}`;
+                let population = data.population;
+                let region = data.region;
+                let capital = data.capital;
+                let flag_url = data.flags.png
+                // Create main article tag for country
+                const article = document.createElement('article');
+                article.className = 'country-card';
+                document.querySelector('div.articles-container').appendChild(article);
+
+                // Load flag image
+                const div_flag = document.createElement('div');
+                div_flag.className = 'card';        
+                article.appendChild(div_flag);
+                div_flag.style.backgroundImage="url(" + flag_url + ")";
+
+                // Load country name
+                const div_name = document.createElement('div');
+                div_name.className = 'country-name'; 
+                div_name.appendChild(document.createTextNode(`${country}`));       
+                article.appendChild(div_name);
+
+                // Load country population
+                const div_population = document.createElement('div');
+                div_population.className = 'country-population'; 
+                //Add "," in thousands place
+                div_population.appendChild(document.createTextNode(`${population.toLocaleString('en-US')}`));       
                 article.appendChild(div_population);
 
                 // Load country region
